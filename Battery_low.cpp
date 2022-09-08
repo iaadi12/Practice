539f5acdc78144a66b55c566be303919f4f2f119
Aaditya Kumar Sharma <97457902+iaadi12@users.noreply.github.com>
2022-09-08 22:08:08 +0530

    Create Battery_low.cpp

---
+++ Battery_low.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,X;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>X;
+	    if(X<=15)
+	    {
+	        cout<<"Yes\n";
+	    }
+	    else 
+	        cout<<"No\n";
+	}
+	return 0;
+}
