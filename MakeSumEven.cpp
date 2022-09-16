01ed78fb91978f47cfcce2dedb4361462cf1d0d3
Aaditya Kumar Sharma <97457902+iaadi12@users.noreply.github.com>
2022-09-16 18:51:41 +0530

    Create MakeSumEven.cpp

---
+++ MakeSumEven.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    bool makeEven(vector<int> arr) {
+        int sum=0,n=arr.size()-1;
+        int even=0,odd=0;
+        if(n%2==0)
+        return false;
+        for(int i=0; i<arr.size();i++)
+        {
+            if(arr[i]%2==0)
+                even++;
+            else
+                odd++;
+        }
+        if(even % 2 == 0 && odd % 2 == 0)
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+};
