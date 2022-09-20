c156c56a950dccc82f62f586eb1634d9c092b97b
Aaditya Kumar Sharma <97457902+iaadi12@users.noreply.github.com>
2022-09-20 22:41:06 +0530

    Update Buy_and_Sell_Stocks.cpp

--- Buy_and_Sell_Stocks.cpp
+++ Buy_and_Sell_Stocks.cpp
@@ -35,3 +35,23 @@ public:
         
     }
 };
+
+
+Solution 3---->
+ class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int maxPro=0;
+        int buy=prices[0];
+        for(int i = 0; i < n; i++)
+        {
+             if(prices[i] < buy)
+                    buy=prices[i];
+             else
+                   maxPro=max(prices[i] - buy,maxPro);
+        }
+        return maxPro;
+        
+    }
+};
