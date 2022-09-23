9cc2bfe89ed0fdf4f421ca59cc10f7d0ecc31787
Aaditya Kumar Sharma <97457902+iaadi12@users.noreply.github.com>
2022-09-23 22:04:40 +0530

    Create First_unique_char.cpp

---
+++ First_unique_char.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int len=s.length();
+        map<char,int>mpp;
+        int add=0;
+        for(int i=0;i<len;i++)
+        {
+            mpp[s[i]]++;
+            
+        }
+        for(int i=0; i<len;i++)
+        {
+            if(mpp[s[i]]==1)
+                return i;
+        }
+        return -1;
+    }
+};
