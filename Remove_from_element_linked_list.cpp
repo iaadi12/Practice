3667e7161db2b1fbea3c468beabde5765fc3c593
Aaditya Kumar Sharma <97457902+iaadi12@users.noreply.github.com>
2022-09-26 22:47:10 +0530

    Update Remove_from_element_linked_list.cpp

--- Remove_from_element_linked_list.cpp
+++ Remove_from_element_linked_list.cpp
@@ -20,3 +20,43 @@ public:
         return head;
     }
 };
+
+
+Solution2-->
+  
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode *p,*q;
+        p=head;
+        q=NULL;
+        if(!head)
+            return NULL;
+        while(head!=NULL && head->val==val)
+            head=head->next;
+        while(p!=NULL)
+        {
+            q=p;
+            p=p->next;
+            if(p!=NULL && p->val==val){
+                q->next=p->next; 
+                p=q;
+            }  
+        }
+        return head;
+    }
+};
+
+Solution3--->
+
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if(!head)
+            return NULL;
+        head->next=removeElements(head->next,val);
+        if(head->val==val)
+            return head->next;
+        return head;
+    }
+};
