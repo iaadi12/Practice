90466799914e20428bb6997be9cc81dfa6d1d8a3
Aaditya Kumar Sharma <97457902+iaadi12@users.noreply.github.com>
2022-09-30 21:18:59 +0530

    Update Insert_BST.cpp

--- Insert_BST.cpp
+++ Insert_BST.cpp
@@ -13,4 +13,32 @@ public:
 
 Solution 2--->
   
-  
+  class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root == NULL)
+        {
+            return new TreeNode(val);
+        }
+        TreeNode *r=NULL;
+        TreeNode *rt=root;
+        while(rt != NULL)
+        {
+            r=rt;
+            if(val == rt->val)
+                return NULL;
+            else if(val < rt->val)
+                rt = rt->left;
+            else
+                rt = rt->right;
+        }
+        TreeNode* p = new TreeNode;
+        p->val=val;
+        p->left=p->right=NULL;
+        if(p->val < r->val)
+            r->left = p;
+        else
+            r->right = p;
+        return root;
+    }
+};
