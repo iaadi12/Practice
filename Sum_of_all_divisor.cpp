9b57fe02204585a5ffa193102df12f0eebdc73f8
Aaditya Kumar Sharma <97457902+iaadi12@users.noreply.github.com>
2022-11-08 13:52:50 +0530

    Update Sum_of_all_divisor.cpp

--- Sum_of_all_divisor.cpp
+++ Sum_of_all_divisor.cpp
@@ -1,18 +1,37 @@
-class Solution {
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> res;
-        int n=nums.size();
-        for(int i = 0; i< n; i++)
+    long long sumOfDivisors(int N)
+    {
+        // Write Your Code here
+        long sum=0;
+        for(int i = 1; i <= N; ++i)
         {
-            for(int j = i + 1;j < n;j++){
-                if(nums[i] + nums[j] == target){
-                    res.push_back(i);
-                    res.push_back(j);
-                    break;
-            }
-            }
+            sum+=(N/i)*i;
         }
-        return res;
+        return sum;
     }
 };
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        long long ans  = ob.sumOfDivisors(N);
+        cout<<ans<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
