02a7148e969a9ef8208f5373c284acd0e30416a9
Aaditya Kumar Sharma <97457902+iaadi12@users.noreply.github.com>
2022-11-08 20:01:20 +0530

    Create kth_largest_element.cpp

---
+++ kth_largest_element.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+      priority_queue<int>pq(nums.begin(),nums.end());
+      int ans=0;
+      while(k--)
+      {
+        ans=pq.top();
+        pq.pop();
+      }
+      return ans;
+    }
+};
+
+Solution 2--->
+  class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+      sort(nums.begin(),nums.end());
+      int n=nums.size();
+      return nums[n-k];
+      
+    }
+};
+
+Solution 3-->
+  class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+    priority_queue<int,vector<int>,greater<int>>pq;
+    for(auto x:nums)
+    {
+      pq.push(x);
+      if(pq.size()<k)
+        pq.pop();
+    }
+      
+      return pq.top();
+    }
+};
