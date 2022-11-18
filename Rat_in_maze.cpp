803e3012463bce5b76f8981330a830edc95f3ad1
Aaditya Kumar Sharma <97457902+iaadi12@users.noreply.github.com>
2022-11-18 22:29:57 +0530

    Update Rat_in_maze.cpp

--- Rat_in_maze.cpp
+++ Rat_in_maze.cpp
@@ -1,3 +1,31 @@
+Consider a rat placed at (0, 0) in a square matrix of order N * N. It has to reach the destination at (N - 1, N - 1). Find all possible paths that the rat can take to reach from source to destination. The directions in which the rat can move are 'U'(up), 'D'(down), 'L' (left), 'R' (right). Value 0 at a cell in the matrix represents that it is blocked and rat cannot move to it while value 1 at a cell in the matrix represents that rat can be travel through it.
+Note: In a path, no cell can be visited more than one time. If the source cell is 0, the rat cannot move to any other cell.
+
+Example 1:
+
+Input:
+N = 4
+m[][] = {{1, 0, 0, 0},
+         {1, 1, 0, 1}, 
+         {1, 1, 0, 0},
+         {0, 1, 1, 1}}
+Output:
+DDRDRR DRDDRR
+Explanation:
+The rat can reach the destination at 
+(3, 3) from (0, 0) by two paths - DRDDRR 
+and DDRDRR, when printed in sorted order 
+we get DDRDRR DRDDRR.
+Example 2:
+Input:
+N = 2
+m[][] = {{1, 0},
+         {1, 0}}
+Output:
+-1
+Explanation:
+No path exists and destination cell is 
+blocked.
 //{ Driver Code Starts
 // Initial template for C++
 
@@ -51,6 +79,78 @@ class Solution {
       vector < vector < int >> vis(n, vector < int > (n, 0));
 
       if (m[0][0] == 1) findPathHelper(0, 0, m, n, ans, "", vis);
+      return ans;
+    }
+};
+    
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> m(n, vector<int> (n,0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> m[i][j];
+            }
+        }
+        Solution obj;
+        vector<string> result = obj.findPath(m, n);
+        sort(result.begin(), result.end());
+        if (result.size() == 0)
+            cout << -1;
+        else
+            for (int i = 0; i < result.size(); i++) cout << result[i] << " ";
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+
+Solution 2---//without many if()
+  //{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  void findPathHelper(int i, int j, vector < vector < int >> & a, int n, vector < string > & ans, string move,
+    vector < vector < int >> & vis,int di[],int dj[]) {
+    if (i == n - 1 && j == n - 1) {
+      ans.push_back(move);
+      return;
+    }
+    string dir="DLRU";
+    for(int ind=0; ind<4; ind++){
+        int nexti = i+di[ind];
+        int nextj = j+dj[ind];
+        if (nexti>=0 && nextj>=0 && nexti<n && nextj<n && !vis[nexti][nextj] && a[nexti][nextj] == 1) {
+          vis[i][j] = 1;
+          findPathHelper(nexti,nextj, a, n, ans, move + dir[ind], vis,di,dj);
+          vis[i][j] = 0;
+        }
+    }
+
+  }
+  public:
+    vector < string > findPath(vector < vector < int >> & m, int n) {
+      vector < string > ans;
+      vector < vector < int >> vis(n, vector < int > (n, 0));
+      int di[]={+1,0,0,-1};
+      int dj[]={0,-1,1,0};
+      if (m[0][0] == 1) findPathHelper(0, 0, m, n, ans, "", vis,di,dj);
       return ans;
     }
 };
